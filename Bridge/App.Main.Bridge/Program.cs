33b0cb170ed9667cb71fbbebcefcd7624bfd6310
Madhura Bindu <madhura.bindu@xymob.in>
2021-02-12 14:40:59 +0530

    bridge

---
+++ Bridge/App.Main.Bridge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Base.Interfaces;
+using Imp.Quizzes;
+
+namespace App.Main.Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets Take a Spin The Wheel Quiz.....");
+            IQuiz quiz = new SpinTheWheelQuiz();
+            quiz.TakeQuiz();
+            Console.WriteLine();
+
+
+            Console.WriteLine("Lets Take a TRUE or FALSE Quiz.....");
+            IQuiz quiz2 = new TrueOrFalseQuiz();
+            quiz2.TakeQuiz();
+            Console.WriteLine();
+        }
+    }
+}
